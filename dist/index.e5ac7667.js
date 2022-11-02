16e0074d75a16bd1830573ec34c26d28f68e7bfa
Jimmy <jimmyjunttila@hotmail.com>
2022-11-02 16:26:36 +0100

    Add 5 'card property-values' under all card-images, and fix design on 'bootstrap cards'

--- dist/index.e5ac7667.js
+++ dist/index.e5ac7667.js
@@ -545,19 +545,18 @@ const getCards = async ()=>{
 getCards().then((dataValue)=>{
     let output = "";
     dataValue.data.map((values)=>{
-        output += `<div class="card h-100">
-            <!-- Product details-->
-            <div class="card-body p-4">
+        output += `<div class="card">
+            <img class="card-img-top" src="${values.images.small}" alt="Card image cap">
+            <div class="card-body p-2">
                 <div class="text-center">
-                    <!-- Product name-->
                     <h5 class="fw-bolder">${values.name}</h5>
-                    <!-- Product price-->
-                    $40.00 - $80.00
+                    <p class="card-desc">Expansion: ${values.set.series}</p>
+                    <p class="card-desc">Released: ${values.set.releaseDate}</p>
+                    <p class="mark d-inline-flex pb-0">$${values.cardmarket.prices.trendPrice}</p>
                 </div>
             </div>
-                <!-- Product actions-->
                 <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                    <div class="text-center"><a class="btn btn-info mt-auto" href="#">View options</a></div>
                 </div>
             </div>`;
     });
